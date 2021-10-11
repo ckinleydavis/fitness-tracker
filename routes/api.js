a6f6270714082ebe50984d9fa4d7b9bf515ed6e8
Kinley Davis <d8tascientist@gmail.com>
2021-10-10 21:54:16 -0400

    coding additional api routes

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,21 @@
 const router = require("express").Router();
-const Workout = require("../models/workout.js");
+const Workout = require("../models/workout");
 
 // addExercise()
-router.post('./api/workout', (res) => {
-    Workout.create()
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
+
+// getWorkoutsInRange()
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+        .limit(3)
         .then((dbWorkout) => {
             res.json(dbWorkout);
         })
@@ -18,13 +30,17 @@ router.post({
 });
 
 // getLastWorkout()
-router.get({
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
 
 });
 
-// getWorkoutsInRange()
-router.get({
 
-});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -19,6 +19,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 
 // Routes
 app.use(require("./routes/api"));
+app.use(require("./routes/index"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
